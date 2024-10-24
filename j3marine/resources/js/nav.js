a29ea185bdb05a9c14c07a9c5dc646410c2818e1
j2pitcher <allen.pitcher@gmail.com>
2024-10-24 16:37:26 -0400

    Update nav.js

--- j3marine/resources/js/nav.js
+++ j3marine/resources/js/nav.js
@@ -1,16 +1,11 @@
 // This script will dynamically generate the navigation bar on all pages
 
 // Define the HTML content for the navigation bar
-let baseURL = "/";
-
-if(currentPath.includes("resources/pages/")) {
-    baseURL = "resources/pages/";
-}
 const navHTML = `
     <ul>
-        <li><a href="${baseURL === '/' ? './index.html' : '../../index.html'}"><button>Home Page</button></a></li>
-        <li><a href="${baseURL}parts.html"><button>Parts Review</button></a></li>
-        <li><a href="${baseURL}crm.html"><button>CRM Portal</button></a></li>
+        <li><a href="./index.html"><button>Home Page</button></a></li>
+        <li><a href="resources/pages/parts.html"><button>Parts Review</button></a></li>
+        <li><a href="resources/pages/crm.html"><button>CRM Portal</button></a></li>
         <li><button>Marina Management</button></li>
         <li><button>Employee Portal</button></a></li>
     </ul>
