a3f49508d691de8007407484a3585ee5abcb4114
j2pitcher <allen.pitcher@gmail.com>
2024-10-24 14:08:14 -0400

    Update nav.js

--- j3marine/resources/js/nav.js
+++ j3marine/resources/js/nav.js
@@ -3,9 +3,9 @@
 // Define the HTML content for the navigation bar
 const navHTML = `
     <ul>
-        <li><a href="/j3marine/index.html"><button>Home Page</button></a></li>
-        <li><a href="/j3marine/resources/pages/parts.html"><button>Parts Review</button></a></li>
-        <li><a href="/j3marine/resources/pages/crm.html"><button>CRM Portal</button></a></li>
+        <li><a href="./j3marine/index.html"><button>Home Page</button></a></li>
+        <li><a href="./j3marine/resources/pages/parts.html"><button>Parts Review</button></a></li>
+        <li><a href="./j3marine/resources/pages/crm.html"><button>CRM Portal</button></a></li>
         <li><button>Marina Management</button></li>
         <li><button>Employee Portal</button></a></li>
     </ul>
